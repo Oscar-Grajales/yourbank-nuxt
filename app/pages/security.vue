b8d86fc1d42a295e1c01c7a58a3fbde3f926fd0c
oscargg <ograjales4@gmail.com>
2026-04-19 13:26:39 -0600

    feat(security): add Hero section

--- app/pages/security.vue
+++ app/pages/security.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+  <main>
+    <Hero
+      title="security.hero.title"
+      highlight="security.hero.highlight"
+      description="security.hero.description"
+      :image-src="SecurityHeroImg"
+      image-alt="security.hero.image_alt"
+    />
+  </main>
+</template>
+
+<script setup lang="ts">
+import Hero from '~/components/shared/Hero.vue';
+import SecurityHeroImg from '~/assets/img/hero/security.webp'
+</script>
