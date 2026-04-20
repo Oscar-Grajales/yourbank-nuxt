d4682508bff23e6351f68520cd8ae3e38ef128c3
oscargg <ograjales4@gmail.com>
2026-04-20 09:22:47 -0600

    feat(security): add Protection section

---
+++ app/components/security/Protection.vue
@@ -0,0 +1,60 @@
+<template>
+  <Section>
+    <SectionHeader
+      :description="$t('security.protection.header.description')"
+      class="max-w-7xl"
+    >
+      <template #title>
+        <i18n-t keypath="security.protection.header.title" tag="span" scope="global">
+          <template v-slot:highlight>
+            <span class="text-primary">{{ $t('security.protection.header.highlight') }}</span>
+          </template>
+        </i18n-t>
+      </template>
+    </SectionHeader>
+    <div class="mt-25 grid grid-cols-2 gap-7.5">
+      <div
+        v-for="feature in features"
+        :key="feature.key"
+        class="p-12.5 bg-gray-11 border border-gray-15 rounded-[20px]"
+      >
+        <div class="mb-6 flex items-center gap-5">
+          <GlowIconContainer>
+            <component :is="feature.icon" />
+          </GlowIconContainer>
+          <h3 class="text-2xl">{{ $t(`security.protection.features.${feature.key}.title`) }}</h3>
+        </div>
+        <p class="text-gray-70 font-light">{{ $t(`security.protection.features.${feature.key}.description`) }}</p>
+      </div>
+    </div>
+  </Section>
+</template>
+
+<script setup lang="ts">
+import CurrencyDollarCircleIcon from '../icons/filled/CurrencyDollarCircleIcon.vue';
+import SparklesIcon from '../icons/filled/SparklesIcon.vue';
+import Square3StackIcon from '../icons/filled/Square3StackIcon.vue';
+import SquaresPlusIcon from '../icons/filled/SquaresPlusIcon.vue';
+import Section from '../layout/Section.vue';
+import SectionHeader from '../layout/SectionHeader.vue';
+import GlowIconContainer from '../ui/GlowIconContainer.vue';
+
+const features = [
+  {
+    key: 'secure_platform',
+    icon: SquaresPlusIcon,
+  },
+  {
+    key: 'mfa',
+    icon: Square3StackIcon,
+  },
+  {
+    key: 'fraud_monitoring',
+    icon: SparklesIcon,
+  },
+  {
+    key: 'secure_app',
+    icon: CurrencyDollarCircleIcon,
+  },
+]
+</script>
--- app/pages/security.vue
+++ app/pages/security.vue
@@ -7,10 +7,12 @@
       :image-src="SecurityHeroImg"
       image-alt="security.hero.image_alt"
     />
+    <Protection />
   </main>
 </template>
 
 <script setup lang="ts">
 import Hero from '~/components/shared/Hero.vue';
 import SecurityHeroImg from '~/assets/img/hero/security.webp'
+import Protection from '~/components/security/Protection.vue';
 </script>
